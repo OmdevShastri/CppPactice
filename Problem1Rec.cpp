855370438b6749ce18649ca5dfa4085a3960f088
Omdev Shastri <omdevsh19ete@student.mes.ac.in>
2021-09-29 18:50:32 +0530

    29thofSept

--- Problem1Rec.cpp
+++ Problem1Rec.cpp
@@ -35,15 +35,80 @@ void towerofHanoi(int n, char src, char dest, char helper){
     cout<<"Move from "<<src<<" to "<<dest<<endl;
     towerofHanoi(n-1,helper, dest,src);
 }
+string removeDup(string s){
+    if(s.length()==0){
+        return "";
+    }
+    char ch=s[0];
+    string ans = removeDup(s.substr(1));
+
+    if (ch==ans[0])
+    {
+        return ans;
+    }
+    return (ch+ans);
+}
+string moveAllX(string s){
+    if (s.length()==0)
+    {
+        return "";
+    }
+    char ch=s[0];
+    string ans=moveAllX(s.substr(1));
+    if (ch=='x')
+    {
+        return ans+ch;
+    }
+    return ch+ans;
+}
+void subseq(string s, string ans){
+    if (s.length()==0)
+    {
+        cout<<ans<<endl;
+        return;
+    }
+    char ch=s[0];
+    int code=ch;//to print ascii values
+
+    string ros=s.substr(1);
+
+    subseq(ros, ans);
+    subseq(ros,ans+ch);
+    subseq(ros, ans +to_string(code));
+ 
+}
+string keypadArr[]={"", "","abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+void keypad(string s, string ans){
+    if (s.length()==0)
+    {
+        cout<<ans<<endl;
+        return;
+    }
+    char ch=s[0];
+    string code= keypadArr[ch-'0'];
+    string ros=s.substr(1);
+
+    for (int i = 0; i < code.length(); i++)
+    {
+        keypad(ros,ans+code[i]);
+    }
+    
+}
+
 int main(){
-    string s;
-    cin>>s;
-    rev(s);
-    cout<<endl;
-    replacePi("pippppiiiipi");
-    cout<<endl;
+    // string s;
+    // cin>>s;
+    // rev(s);
+    // cout<<endl;
+    // replacePi("pippppiiiipi");
+    // cout<<endl;
 
-    towerofHanoi(3,'A','C','B');
-
+    // towerofHanoi(3,'A','C','B');
+    //cout<<removeDup("aaaabbbeeeeeeccccccddddddd")<<endl;
+    //cout<<moveAllX("axxxsxsxsdesewxxxy")<<endl;
+    //subseq("ABC","");
+    //subseq("AB","");
+    //cout<<endl;
+    keypad("23","");
     return 0;
 }
